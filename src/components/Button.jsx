37061d4633707b4f9aa94ce791b36a61866437bf
Quayyum <quayyumariyo@gmail.com>
2025-07-03 08:44:23 +0100

    chore: update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,13 @@ const Button = () => {
   return (
     <a className="cta-wrapper">
       <div className="cta-group group">
-      <div></div>
+      <div className="bg-circle"/>
+      <p className="text">
+        See my Work
+      </p>
+      <div className="arrow-wrapper">
+        <img src="images/arrow-down.svg" alt="arrow" />
+      </div>
       </div>
     </a>
   )
