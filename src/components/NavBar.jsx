8ba14b477ba5a7b3652535651565e1e9bb2e9652
Quayyum <quayyumariyo@gmail.com>
2025-07-03 16:44:14 +0100

    Add NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header className='navbar'>
+      <div className='inner'>
+        <a className= "logo" href="#hero">
+        Quayyum Ariyo
+        </a>
+
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
