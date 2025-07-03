5731578f27d0e6ca952ed054de1e97d886d02478
Quayyum <quayyumariyo@gmail.com>
2025-07-03 15:40:57 +0100

    Update ShowcaseSection.jsx layout and content

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const ShowcaseSection = () => {
   return (
-    <div>
-      
+    <div id="work" className='app-showcase'>
+      <div className='w-full'> 
+        <div className= "showcaselayout">
+        {/* LEFT */}
+        <div className= "first-project-wrapper">
+          <div className="image-wrapper">
+            <img src= "/images/project1.png" alt="Ryde"/>
+          </div>
+          <div className= "text-content" >
+            <h2>On-Demand Rides Made Simple with a Powerful, User-Friendly App called Ryde
+            </h2>
+            <p className = "text-white-50 md:text-xl">
+              An app built with R
+              </p>
+        </div>
+        </div>
+        {/* RIGHT */}
+        </div>
+      </div>
     </div>
   )
 }
