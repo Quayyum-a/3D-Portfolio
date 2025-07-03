e1673aae096cd0a5d25edbb1dd8aae06798df9b7
Quayyum <quayyumariyo@gmail.com>
2025-07-03 11:23:37 +0100

    Update HeroLights.jsx: Adjust lighting and add RectAreaLight

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -1,8 +1,34 @@
+import * as THREE from "three";
+
 const HeroLights = () => {
   return (
     <>
-      <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <spotLight
+      position={[2,5,6]}
+      angle={0.15}
+      intensity={100}
+      penumbra={0.2}
+      color="white"
+      />
+
+      <spotLight
+      position={[4,5,4]}
+      angle={0.3}
+      intensity={40}
+      penumbra={0.5}
+      color="#4cc9f0"
+      />
+      <spotLight
+      position={[-3,5,5]}
+      angle={0.4}
+      intensity={60}
+      penumbra={1}
+      color="#9d4edd"
+      />
+
+      <primitive
+      object={new THREE.RectAreaLight('#A259FF')}
+      />
     </>
   );
 }
