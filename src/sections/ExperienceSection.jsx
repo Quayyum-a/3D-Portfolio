303002ba106f59ef93030567cbce10f5750df101
Quayyum <quayyumariyo@gmail.com>
2025-07-04 14:44:29 +0100

    Update ExperienceSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -14,39 +14,38 @@ const ExperienceSection = () => {
           title="Professional Work Experience"
           sub="💼 My Career Overview"
         />
-        <div className='mt-32 relative'>
-        <div className='relative z-50 xl:space-y-32 space-y-10'>
-          {expCards.map((card, index) => (
-            <div key={card.title} className='exp-card-wrapper'>
-              <div className='xl:w-2/6'>
-                <GlowCard card={card} index={index}>
-                  <div>
-                    <img src={card.imgPath} alt={card.title} />
-                  </div>
-                </GlowCard>
-              </div>
-              <div className='xl:w-4/6'>
-              <div className='flex items-start'>
-                <div className='timeline-wrapper'>
-                  <div className='timeline'/>
-                  <div className='gradient-line w-1 h-full'/>
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {expCards.map((card, index) => (
+              <div key={card.title} className="exp-card-wrapper">
+                <div className="xl:w-2/6">
+                  <GlowCard card={card} index={index}>
+                    <div>
+                      <img src={card.imgPath} alt={card.title} />
+                    </div>
+                  </GlowCard>
                 </div>
-                <div className='expText flex xl:gap-20 md:gap-10 gap-5 relative z-20'>
-                  <div className='timeline-logo'>
-                      <img src={card.logoPath} alt="logo" />
-                  </div>
-                  <div>
-                    <h1 className='font-semibold text-3xl'>{card.title}</h1>
-                    <p className='my-5 text-white-50'>
-                      {card.date}
-                    </p>
+                <div className="xl:w-4/6">
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      <div className="timeline" />
+                      <div className="gradient-line w-1 h-full" />
+                    </div>
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                        <img src={card.logoPath} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-3xl">{card.title}</h1>
+                        <p className="my-5 text-white-50"> 📅 {card.date}</p>
+                        <p className='text-[#839cb5] italic '> Responsibilities </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
