a60b3bb4710e3178a66467c53a9f2fefa24f3752
Quayyum <quayyumariyo@gmail.com>
2025-07-04 07:55:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import LogoSection from "./components/LogoSection";
+import LogoSection from "./sections/LogoSection";
 import NavBar from "./components/NavBar";
 import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero"
@@ -10,7 +10,7 @@ const App = () => {
     <NavBar/>
       <Hero />
       <ShowcaseSection/>
-      {/* <LogoSection/> */}
+      <LogoSection/>
       <FeatureCards/>
     </>
   );
