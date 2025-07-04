e6f04879b905db3e9de02784611c2a01884f3722
Quayyum <quayyumariyo@gmail.com>
2025-07-04 07:41:23 +0100

    Update FeatureCards.jsx

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { abilities } from '../constants'
+import { div } from 'three/tsl'
 
 const FeatureCards = () => {
   return (
-    <div className='w-full padding-g-lg'>
-    <div className='mx-auto grid-3-col'>
-      {abilities}
-    </div> 
+    <div className="w-full padding-g-lg">
+      <div className="mx-auto grid-3-col">
+        {abilities.map(({ imgPath, title, desc }) => (
+          <div key={title} className="card-border rounded-xl p-8 flex flex-col gap-4">
+            <div>
+              
+            </div>
+            <h3>{title}</h3>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default FeatureCards
