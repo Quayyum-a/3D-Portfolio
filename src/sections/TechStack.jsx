150fd8fc096dd667c550dd1fea78e7a801f88de4
Quayyum <quayyumariyo@gmail.com>
2025-07-05 10:29:29 +0100

    TechStack: first commit for changes

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { techStackIcons } from '../constants';
+import TechIcon from '../components/Models/TechLogos/TechIcon';
 
 const TechStack = () => {
   return (
@@ -10,16 +11,24 @@ const TechStack = () => {
           title="My Preferred Tech Stack"
           sub="🤝 The Skills I Bring to the Table "
         />
-        <div className="tech-grid">{techStackIcons.map((icon) => (
-          <div key={icon.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
-            <div className='tech-card-animated-bg'/>
-            <div className='tech-card-content'>
-              <div className='tech-icon-wrapper'>
-
+        <div className="tech-grid">
+          {techStackIcons.map((icon) => (
+            <div
+              key={icon.name}
+              className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+            >
+              <div className="tech-card-animated-bg" />
+              <div className="tech-card-content">
+                <div className="tech-icon-wrapper">
+                  <TechIcon model={icon} />
+                </div>
+                <div className="padding-x w-full">
+                  <p>{icon.name}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
