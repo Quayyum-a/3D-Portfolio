05428a787464a1e14f59e5ed98a55bf6a685ffd9
Quayyum <quayyumariyo@gmail.com>
2025-07-05 10:15:34 +0100

    Tech stack added

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
+import { techStackIcons } from '../constants';
 
 const TechStack = () => {
   return (
-    <div id="skills" className='flex-center section-padding'>
-      <div className='w-full h-full md:px-10 px-5'>
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
         <TitleHeader
-        title="My Preferred Tech Stack"
-        sub="🤝 The Skills I Bring to the Table "
+          title="My Preferred Tech Stack"
+          sub="🤝 The Skills I Bring to the Table "
         />
+        <div className="tech-grid">{techStackIcons.map((icon))}</div>
       </div>
     </div>
-  )
+  );
 }
 
 export default TechStack
