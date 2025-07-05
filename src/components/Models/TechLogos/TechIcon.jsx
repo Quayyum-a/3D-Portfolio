dfa041d03f65cca0f311fb310baff3c8014dfaac
Quayyum <quayyumariyo@gmail.com>
2025-07-05 10:32:41 +0100

    TechIcon.jsx: first commit for staged changes

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -1,10 +1,13 @@
+import { useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
-const TechIcon = () => {
+const TechIcon = ({model }) => {
+  const scene = useGLTF(model.modelPath)
   return (
-    <div>
-      
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.3}/>
+    </Canvas>
   )
 }
 
