f94724a1b043a1f0713f0459da45af96f7eb8d6f
Quayyum <quayyumariyo@gmail.com>
2025-07-06 13:10:22 +0100

    feat: initial commit for new Contact section

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader title="Let's Connect!" sub="📬 Get in Touch With Me" />
+        <div className="mt-16">
+
+        </div>
+        {/* Contact form or details can go here */}
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
