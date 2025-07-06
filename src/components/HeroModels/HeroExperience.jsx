153f4d664f1b11a1a8f6f08fd4d5b9761d7bda4e
Quayyum <quayyumariyo@gmail.com>
2025-07-06 14:53:51 +0100

    feat: update HeroExperience.jsx

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,17 +1,17 @@
-import { OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import { useMediaQuery } from 'react-responsive';
-import { Room } from './Room';
-import HeroLights from './HeroLights';
-import Particles from './Particles';
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import { Room } from "./Room";
+import HeroLights from "./HeroLights";
+import Particles from "./Particles";
 
 const HeroExperience = () => {
-  const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
-  const isMobile = useMediaQuery({query: '(max-width: 768px)'});
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-            <OrbitControls
+      <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
         maxDistance={20}
@@ -20,18 +20,18 @@ const HeroExperience = () => {
         maxPolarAngle={Math.PI / 2}
       />
 
-      <HeroLights/>
+      <HeroLights />
 
-      <Particles count={1000}/>
+      <Particles count={1000} />
       <group
-      scale= {isMobile? 0.7 : 1}
-      position={[0, -3.5, 0]}
-      rotation={[0, -Math.PI/4, 0]}
+        scale={isMobile ? 0.7 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
       >
         <Room />
       </group>
     </Canvas>
   );
-}
+};
 
-export default HeroExperience
+export default HeroExperience;
